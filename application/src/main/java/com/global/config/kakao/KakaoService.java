5511830b01faaadcf184a5b80e8859ca4393a14b
ay-eonii <naayen@naver.com>
2025-07-08 20:45:09 +0900

    refactor: 사용하지 않는 코드 삭제

--- application/src/main/java/com/global/config/kakao/KakaoService.java
+++ application/src/main/java/com/global/config/kakao/KakaoService.java
@@ -1,120 +1,64 @@
 package com.global.config.kakao;
 
-import com.global.config.kakao.dto.KakaoInfoResponse;
 import org.springframework.beans.factory.annotation.Value;
-import com.nimbusds.jose.shaded.gson.JsonElement;
-import com.nimbusds.jose.shaded.gson.JsonParser;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import com.global.config.kakao.dto.KakaoTokenResponse;
+import com.global.config.kakao.dto.KakaoUserInfoResponse;
+import com.global.config.kakao.exception.KakaoTokenException;
+import com.global.config.kakao.exception.KakaoUserInfoException;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
 public class KakaoService {
-    private final String CLIENT_ID;
-    private final String REDIRECT_URL;
-    public KakaoService(@Value("${spring.application.kakao-client-id}") String clientId, @Value("${spring.application.kakao-redirect-uri}") String redirectUrl) {
-        this.CLIENT_ID = clientId;
-        this.REDIRECT_URL = redirectUrl;
-    }
 
-    public String getKakaoAccessToken (String code) {
-        String access_Token = "";
-        String refresh_Token = "";
-        String reqURL = "https://kauth.kakao.com/oauth/token";
+	private final WebClient webClient = WebClient.create();
 
-        try {
-            URL url = new URL(reqURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	@Value("${kakao.token_uri}")
+	private String tokenUri;
 
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
+	@Value("${kakao.redirect_uri}")
+	private String redirectUri;
 
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
-            StringBuilder sb = new StringBuilder();
-            sb.append("grant_type=authorization_code");
-            sb.append("&client_id=" + CLIENT_ID);
-            sb.append("&redirect_uri=" + REDIRECT_URL);
-            sb.append("&code=" + code);
-            bw.write(sb.toString());
-            bw.flush();
+	@Value("${kakao.grant_type}")
+	private String grantType;
 
-            int responseCode = conn.getResponseCode();
-            log.info("responseCode : " + responseCode);
+	@Value("${kakao.client_id}")
+	private String clientId;
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line = "";
-            String result = "";
+	@Value("${kakao.user_info_uri}")
+	private String userInfoUri;
 
-            while ((line = br.readLine()) != null) {
-                result += line;
-            }
+	public KakaoTokenResponse getToken(String code) {
+		String uri = UriComponentsBuilder.fromHttpUrl(tokenUri)
+			.queryParam("grant_type", grantType)
+			.queryParam("client_id", clientId)
+			.queryParam("redirect_uri", redirectUri)
+			.queryParam("code", code)
+			.toUriString();
 
-            log.info("response body : " + result);
+		return webClient.post()
+			.uri(uri)
+			.contentType(MediaType.APPLICATION_JSON)
+			.retrieve()
+			.bodyToFlux(KakaoTokenResponse.class)
+			.onErrorMap(WebClientResponseException.class,
+				e -> new KakaoTokenException()).blockFirst();
+	}
 
-            JsonParser parser = new JsonParser();
-            JsonElement element = parser.parse(result);
-
-            access_Token = element.getAsJsonObject().get("access_token").getAsString();
-            refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
-
-            log.info("access_token : " + access_Token);
-            log.info("refresh_token : " + refresh_Token);
-
-            br.close();
-            bw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return access_Token;
-    }
-    public KakaoInfoResponse getInfo(String token) throws Exception {
-        String reqURL = "https://kapi.kakao.com/v2/user/me";
-
-        URL url = new URL(reqURL);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-        conn.setRequestMethod("POST");
-        conn.setDoOutput(true);
-        conn.setRequestProperty("Authorization", "Bearer " + token);
-
-        int responseCode = conn.getResponseCode();
-        log.info("responseCode : " + responseCode);
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String line = "";
-        String result = "";
-
-        while ((line = br.readLine()) != null) {
-            result += line;
-        }
-        log.info("response body : " + result);
-
-        JsonParser parser = new JsonParser();
-        JsonElement element = parser.parse(result);
-
-        int id = element.getAsJsonObject().get("id").getAsInt();
-        boolean hasEmail = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("has_email").getAsBoolean();
-        String email = "";
-        if(hasEmail){
-            email = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("email").getAsString();
-        }
-        String name = element.getAsJsonObject().get("properties").getAsJsonObject().get("nickname").getAsString();
-
-        log.info("id : " + id);
-        log.info("email : " + email);
-        log.info("nickname : " + name);
-        br.close();
-
-        KakaoInfoResponse kakaoInfoResponse = KakaoInfoResponse.builder()
-                .email(email)
-                .name(name)
-                .build();
-
-        return kakaoInfoResponse;
-    }
+	public KakaoUserInfoResponse getUserInfo(String token) {
+		return webClient.get()
+			.uri(userInfoUri)
+			.header("Authorization", "Bearer " + token)
+			.retrieve()
+			.bodyToFlux(KakaoUserInfoResponse.class)
+			.onErrorMap(WebClientResponseException.class,
+				e -> new KakaoUserInfoException()).blockFirst();
+	}
 }
--- application/src/main/java/com/user/application/usecase/UserManageUsecase.java
+++ application/src/main/java/com/user/application/usecase/UserManageUsecase.java
@@ -1,94 +1,96 @@
 package com.user.application.usecase;
 
+import org.springframework.data.util.Pair;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.global.config.jwt.JwtProvider;
+import com.global.config.jwt.presentation.JwtResponse;
+import com.global.config.kakao.KakaoService;
+import com.global.config.kakao.dto.KakaoAccount;
+import com.global.config.kakao.dto.KakaoTokenResponse;
+import com.global.config.kakao.dto.KakaoUserInfoResponse;
 import com.user.application.dto.response.UserResponseDTO;
 import com.user.application.mapper.UserMapper;
 import com.user.domain.entity.User;
 import com.user.domain.service.UserGetService;
 import com.user.domain.service.UserSaveService;
-import com.global.config.jwt.JwtProvider;
-import com.global.config.jwt.presentation.JwtResponse;
-import com.global.config.kakao.KakaoServiceNew;
-import com.global.config.kakao.dto.KakaoAccount;
-import com.global.config.kakao.dto.KakaoTokenResponse;
-import com.global.config.kakao.dto.KakaoUserInfoResponse;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.util.Pair;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class UserManageUsecase {
 
-    private final UserGetService userGetService;
-    private final UserSaveService userSaveService;
-    private final KakaoServiceNew kakaoServiceNew;
-    private final JwtProvider jwtProvider;
-    private final UserMapper userMapper;
+	private final UserGetService userGetService;
+	private final UserSaveService userSaveService;
+	private final KakaoService kakaoService;
+	private final JwtProvider jwtProvider;
+	private final UserMapper userMapper;
 
-    public UserResponseDTO.Response authenticate(String code) {
-        KakaoTokenResponse tokenResponse = kakaoServiceNew.getToken(code);
-        KakaoUserInfoResponse userInfo = kakaoServiceNew.getUserInfo(tokenResponse.getAccess_token());
+	public UserResponseDTO.Response authenticate(String code) {
+		KakaoTokenResponse tokenResponse = kakaoService.getToken(code);
+		KakaoUserInfoResponse userInfo = kakaoService.getUserInfo(tokenResponse.getAccess_token());
 
-        return registerMemberIfNew(userInfo);
-    }
+		return registerMemberIfNew(userInfo);
+	}
 
-    public UserResponseDTO.Response reissueToken(String token) {
-        Pair<Long, String> pair = validateRefreshToken(token);
+	public UserResponseDTO.Response reissueToken(String token) {
+		Pair<Long, String> pair = validateRefreshToken(token);
 
-        long userId = pair.getFirst();
-        String refreshToken = pair.getSecond();
+		long userId = pair.getFirst();
+		String refreshToken = pair.getSecond();
 
-        User user = userGetService.find(userId);
-        user.checkRefreshToken(refreshToken);
+		User user = userGetService.find(userId);
+		user.checkRefreshToken(refreshToken);
 
-        JwtResponse tokenDto = getTokenDto(user);
-        user.updateRefreshToken(tokenDto.getRefreshToken());
+		JwtResponse tokenDto = getTokenDto(user);
+		user.updateRefreshToken(tokenDto.getRefreshToken());
 
-        return userMapper.toResponseDTO(user, tokenDto);
-    }
+		return userMapper.toResponseDTO(user, tokenDto);
+	}
 
-    private UserResponseDTO.Response registerMemberIfNew(KakaoUserInfoResponse userInfo) {
-        String email = userInfo.getKakao_account().getEmail();
+	private UserResponseDTO.Response registerMemberIfNew(KakaoUserInfoResponse userInfo) {
+		String email = userInfo.getKakao_account().getEmail();
 
-        if (userGetService.existsByEmail(email)) {
-            return getUserResponse(email);
-        }
+		if (userGetService.existsByEmail(email)) {
+			return getUserResponse(email);
+		}
 
-        return registerNewUser(userInfo.getKakao_account());
-    }
+		return registerNewUser(userInfo.getKakao_account());
+	}
 
-    private UserResponseDTO.Response registerNewUser(KakaoAccount account) {
-        User user = userMapper.from(account);
-        userSaveService.save(user);
+	private UserResponseDTO.Response registerNewUser(KakaoAccount account) {
+		User user = userMapper.from(account);
+		userSaveService.save(user);
 
-        JwtResponse tokenDto = getTokenDto(user);
-        user.updateRefreshToken(tokenDto.getRefreshToken());
+		JwtResponse tokenDto = getTokenDto(user);
+		user.updateRefreshToken(tokenDto.getRefreshToken());
 
-        return userMapper.toResponseDTO(user, tokenDto);
-    }
+		return userMapper.toResponseDTO(user, tokenDto);
+	}
 
-    private UserResponseDTO.Response getUserResponse(String email) {
-        User user = userGetService.findByEmail(email);
+	private UserResponseDTO.Response getUserResponse(String email) {
+		User user = userGetService.findByEmail(email);
 
-        JwtResponse tokenDto = getTokenDto(user);
-        user.updateRefreshToken(tokenDto.getRefreshToken());
+		JwtResponse tokenDto = getTokenDto(user);
+		user.updateRefreshToken(tokenDto.getRefreshToken());
 
-        return userMapper.toResponseDTO(user, tokenDto);
-    }
+		return userMapper.toResponseDTO(user, tokenDto);
+	}
 
-    private JwtResponse getTokenDto(User user) {
-        return new JwtResponse(
-                jwtProvider.createAccessToken(user.getId()),
-                jwtProvider.createRefreshToken(user.getId())
-        );
-    }
+	private JwtResponse getTokenDto(User user) {
+		return new JwtResponse(
+			jwtProvider.createAccessToken(user.getId()),
+			jwtProvider.createRefreshToken(user.getId())
+		);
+	}
 
-    private Pair<Long, String> validateRefreshToken(String token) {
-        String refreshToken = jwtProvider.extractRefreshToken(token);
-        Long userId = jwtProvider.extractId(refreshToken);
+	private Pair<Long, String> validateRefreshToken(String token) {
+		String refreshToken = jwtProvider.extractRefreshToken(token);
+		Long userId = jwtProvider.extractId(refreshToken);
 
-        return Pair.of(userId, refreshToken);
-    }
+		return Pair.of(userId, refreshToken);
+	}
 }
